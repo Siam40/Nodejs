9d6b6a215052434440e60af8b970189d66ee91de
Abdus Sattar <48399804+Siam40@users.noreply.github.com>
2023-03-19 19:40:53 +0600

    Os and path module

---
+++ lesson3/index.js
@@ -0,0 +1,38 @@
+// const os = require('os');
+
+const { extname } = require("path");
+
+// const { totalmem, freemem } = require('os');
+
+// console.log(os.userInfo());
+// console.log(os.homedir());
+
+// console.log(os.hostname());
+
+// console.log(os.totalmem());
+// console.log(os.freemem());
+
+
+// console.log(__dirname);
+// console.log(__filename);
+
+
+// const {totalmem, freemem}= require('os');
+
+
+// console.log(totalmem());
+// console.log(freemem());
+
+const path= require ( "path");
+
+// const extensionName = path.extname( "index.html");
+//  console.log(extensionName);
+
+// const joinName = path.join(__dirname + "/views");
+ const joinName = path.join(__dirname + "/../views");
+
+ console.log(joinName);
+//  console.log(joinName);
+
+
+
